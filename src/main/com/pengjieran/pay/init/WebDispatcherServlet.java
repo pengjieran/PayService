d34771f8fdfacdea045a8331233685a85e5e3355
DeanAaron <pengjieran@gmail.com>
2016-03-27 10:04:35 +0800

    为服务器增加log日志

--- src/main/com/pengjieran/pay/init/WebDispatcherServlet.java
+++ src/main/com/pengjieran/pay/init/WebDispatcherServlet.java
@@ -7,10 +7,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.logging.LogManager;
 
 /**
  * Created by aaron on 2016/3/25.
@@ -18,20 +14,11 @@ import java.util.logging.LogManager;
 @WebServlet(name="SpringMvc",urlPatterns={"/*"},initParams={@WebInitParam(name="contextConfigLocation",value="classpath*:**/spring-context.xml")},loadOnStartup=1,asyncSupported=true)
 public class WebDispatcherServlet extends DispatcherServlet implements WebApplicationInitializer {
 
-    static {
-
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        InputStream stream = loader.getResourceAsStream("resources" + File.separator + "logger" + File.separator + "logger.xml");
-        try {
-            LogManager.getLogManager().readConfiguration(stream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
 
 
+
+        System.out.println("初始化系统开始");
     }
 }
