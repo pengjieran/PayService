661556dcc55dc1b02a3ec9d028ca72ae7825b688
DeanAaron <pengjieran@gmail.com>
2016-04-06 22:09:41 +0800

    删除不必要的文件，处理支付宝支付相关的逻辑

---
+++ src/main/java/com/pengjieran/pay/model/AliPayDetail.java
@@ -0,0 +1,8 @@
+package com.pengjieran.pay.model;
+
+/**
+ * Created by Aaron on 2016/4/6.
+ */
+public class AliPayDetail extends PayDetail {
+
+}
--- src/main/java/com/pengjieran/pay/model/PayDetail.java
+++ src/main/java/com/pengjieran/pay/model/PayDetail.java
@@ -4,4 +4,6 @@ package com.pengjieran.pay.model;
  * Created by aaron on 2016/4/6.
  */
 public class PayDetail {
+
+    protected PayDetail() {}
 }
