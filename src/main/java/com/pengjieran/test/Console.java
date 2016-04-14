a6e37fecca7570cfb6367c7970b643e4fc8873a6
Aaron <pengjieran@gmail.com>
2016-04-14 17:30:29 +0800

    增加测试druid生成sql语句的工厂方法

--- src/main/java/com/pengjieran/test/Console.java
+++ src/main/java/com/pengjieran/test/Console.java
@@ -1,6 +1,11 @@
 package com.pengjieran.test;
 
+import com.alibaba.druid.sql.builder.SQLBuilderFactory;
+import com.alibaba.druid.sql.builder.SQLSelectBuilder;
+import com.alibaba.druid.sql.builder.SQLUpdateBuilder;
+import com.alibaba.druid.util.JdbcConstants;
 import com.pengjieran.util.PropertyUtil;
+import org.junit.Test;
 
 import java.io.File;
 import java.util.Properties;
@@ -10,11 +15,28 @@ import java.util.Properties;
  */
 public class Console {
 
-    public static void main (String[] args) {
+    @Test
+    public void main() {
 
         String path = "config" + File.separator + "alipay.properties";
         Properties properties = PropertyUtil.load(path);
         String property = properties.getProperty("alipay.charset");
         System.out.println(property);
+    }
+
+    @Test
+    public void select() {
+
+        SQLSelectBuilder sqlBuilder = SQLBuilderFactory.createSelectSQLBuilder(JdbcConstants.MYSQL);
+
+        sqlBuilder.from("tb1", "t1");
+        String string = sqlBuilder.select("id","name","age").whereAnd("a=3 and b=6").whereAnd("c=5").orderBy("id ASC").toString();
+        System.out.println(string);
+    }
+
+    @Test
+    public void Insert() {
+
+        SQLUpdateBuilder updateBuilder = SQLBuilderFactory.createUpdateBuilder(JdbcConstants.MYSQL);
     }
 }
