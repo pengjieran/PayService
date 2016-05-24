05ad9f3179829b9c8ac9bf9ec932135a87b15dd3
Aaron <pengjieran@gmail.com>
2016-05-24 11:47:02 +0800

    增加springtest测试框架

---
+++ src/test/AreaTest.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by aaron on 2016/5/24.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/spring-context.xml","classpath:spring/spring-web.xml"})
+public class AreaTest {
+}
