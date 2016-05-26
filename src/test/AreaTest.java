416b5770225674ca257f7eed664ca6a0ff7e1949
Aaron <pengjieran@gmail.com>
2016-05-26 19:46:28 +0800

    更新说明文档

--- src/test/AreaTest.java
+++ src/test/AreaTest.java
@@ -1,3 +1,4 @@
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -8,4 +9,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/spring-context.xml","classpath:spring/spring-web.xml"})
 public class AreaTest {
+
+    @Test
+    public void testGet() {
+
+        System.out.println("====");
+    }
 }
